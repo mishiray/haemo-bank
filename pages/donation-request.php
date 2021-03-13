13f95b5253b61c9ad7c09ccc36416fda853f8db2
Okenla Abdul-Basit <okenlabasit@gmail.com>
2021-03-13 18:05:37 +0100

    Added 2 new pages

---
+++ pages/donation-request.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Blood Donation Request Page</title>
+	<link rel="stylesheet" type="text/css" href="../style.css">
+</head>
+<body>
+	<div class="container">
+		<h3 class="text-center">Make a donation request</h3>
+		<form>
+			<div class="row">
+				<div class="col-12">
+				    <label for="inputBloodGroup" class="">Blood Group</label>
+				    <!-- Default option would be the blood group on user's profile -->
+				    <select class="form-select" id="inputBloodGroup" required>
+		                <option value="">Choose...</option>
+		                <option value="o-plus">O+</option>
+		                <option value="o-minus">O-</option>
+		                <option value="a-plus">A+</option>
+		                <option value="a-minus">A-</option>
+		                <option value="b-plus">B+</option>
+		                <option value="b-minus">B-</option>
+		                <option value="ab-plus">AB+</option>
+		                <option value="ab-minus">AB-</option>
+		            </select>
+				</div>
+				<div class="col-12">
+					<label for="inputDate" class="">How soon do you need it?</label>
+					<input type="date" id="inputDate" class="form-control" required>
+				</div>
+				<div class="col-12">
+				    <label for="inputPurpose" class="">Purpose</label>
+				    <textarea id="inputPurpose" class="form-control" required></textarea>
+				</div>
+			</div>
+		    <button class="w-100 mt-2 btn btn-primary btn-lg" type="submit">Submit</button>
+		</form>
+	</div>
+</body>
+</html>
--- pages/register-donor.php
+++ pages/register-donor.php
@@ -1,17 +1,25 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Registration Page</title>
+	<title>Register a Donor Page</title>
 	<link rel="stylesheet" type="text/css" href="../style.css">
 </head>
 <body>
 	<div class="container">
-		<h3 class="text-center">Register with us now</h3>
+		<h3 class="text-center">Register a donor</h3>
 		<form>
 			<div class="row">
+				<div class="col-sm-6">
+				    <label for="inputFirstName" class="">First Name</label>
+				    <input type="text" id="inputFirstName" class="form-control" placeholder="First Name" required>
+				</div>
+				<div class="col-sm-6">
+				    <label for="inputLastName" class="">Last Name</label>
+				    <input type="text" id="inputLastName" class="form-control" placeholder="Last Name" required>
+				</div>
 				<div class="col-12">
-				    <label for="inputName" class="">Full Name</label>
-				    <input type="text" id="inputName" class="form-control" placeholder="Full Name" required>
+				    <label for="inputAge" class="">Age</label>
+				    <input type="number" id="inputAge" class="form-control" required>
 				</div>
 				<div class="col-sm-6">
 					<label for="inputPhoneNumber" class="">Phone Number</label>
@@ -39,14 +47,13 @@
 		                <option value="ab-minus">AB-</option>
 		            </select>
 				</div>
-				<!--
 				<div class="col-12">
 				    <label for="inputOccupation" class="">Occupation</label>
 				    <input type="text" id="inputOccupation" class="form-control" placeholder="" required>
 				</div>
 				
 				<div class="col-12">
-				    <label class="">Have you done a blood donation before?</label>
+				    <label class="">Has donor donated blood before?</label>
 				    <div class="form-check">
 					  <input class="form-check-input" type="radio" name="yesRadio" id="yes">
 					  <label class="form-check-label" for="yes">
@@ -60,7 +67,6 @@
 					  </label>
 					</div>
 				</div>
-				-->
 			</div>
 		    <button class="w-100 mt-2 btn btn-primary btn-lg" type="submit">Submit</button>
 		</form>

22ff18e274909929e1430d0446b168cc17cc26e6
Okenla Abdul-Basit <okenlabasit@gmail.com>
2021-03-22 14:34:12 +0100

    Made changes to the forms

--- pages/donation-request.php
+++ pages/donation-request.php
@@ -7,34 +7,36 @@
 <body>
 	<div class="container">
 		<h3 class="text-center">Make a donation request</h3>
-		<form>
-			<div class="row">
-				<div class="col-12">
-				    <label for="inputBloodGroup" class="">Blood Group</label>
-				    <!-- Default option would be the blood group on user's profile -->
-				    <select class="form-select" id="inputBloodGroup" required>
-		                <option value="">Choose...</option>
-		                <option value="o-plus">O+</option>
-		                <option value="o-minus">O-</option>
-		                <option value="a-plus">A+</option>
-		                <option value="a-minus">A-</option>
-		                <option value="b-plus">B+</option>
-		                <option value="b-minus">B-</option>
-		                <option value="ab-plus">AB+</option>
-		                <option value="ab-minus">AB-</option>
-		            </select>
+		<div class="col-8" style="margin: 0 auto;">
+			<form>
+				<div class="row">
+					<div class="col-12">
+					    <label for="inputBloodGroup" class="">Blood Group</label>
+					    <!-- Default option would be the blood group on user's profile -->
+					    <select class="form-select" id="inputBloodGroup" required>
+			                <option value="">Choose...</option>
+			                <option value="o-plus">O+</option>
+			                <option value="o-minus">O-</option>
+			                <option value="a-plus">A+</option>
+			                <option value="a-minus">A-</option>
+			                <option value="b-plus">B+</option>
+			                <option value="b-minus">B-</option>
+			                <option value="ab-plus">AB+</option>
+			                <option value="ab-minus">AB-</option>
+			            </select>
+					</div>
+					<div class="col-12">
+						<label for="inputDate" class="">How soon do you need it?</label>
+						<input type="date" id="inputDate" class="form-control" required>
+					</div>
+					<div class="col-12">
+					    <label for="inputPurpose" class="">Purpose</label>
+					    <textarea id="inputPurpose" class="form-control" required></textarea>
+					</div>
 				</div>
-				<div class="col-12">
-					<label for="inputDate" class="">How soon do you need it?</label>
-					<input type="date" id="inputDate" class="form-control" required>
-				</div>
-				<div class="col-12">
-				    <label for="inputPurpose" class="">Purpose</label>
-				    <textarea id="inputPurpose" class="form-control" required></textarea>
-				</div>
-			</div>
-		    <button class="w-100 mt-2 btn btn-primary btn-lg" type="submit">Submit</button>
-		</form>
+			    <button class="w-100 mt-2 btn btn-primary btn-lg" type="submit">Submit</button>
+			</form>
+		</div>
 	</div>
 </body>
 </html>
--- pages/main-registration.php
+++ pages/main-registration.php
@@ -7,63 +7,69 @@
 <body>
 	<div class="container">
 		<h3 class="text-center">Register with us now</h3>
-		<form>
-			<div class="row">
-				<div class="col-12">
-				    <label for="inputName" class="">Full Name</label>
-				    <input type="text" id="inputName" class="form-control" placeholder="Full Name" required>
-				</div>
-				<div class="col-sm-6">
-					<label for="inputPhoneNumber" class="">Phone Number</label>
-					<input type="number" id="inputPhoneNumber" class="form-control" placeholder="Phone Number" required>
-				</div>
-				<div class="col-sm-6">
-					<label for="inputEmail" class="">Email Address</label>
-					<input type="email" id="inputEmail" class="form-control" placeholder="Email Address" required>
-				</div>
-				<div class="col-12">
-				    <label for="inputAddress" class="">Address</label>
-				    <textarea id="inputName" class="form-control" required></textarea>
-				</div>
-				<div class="col-12">
-				    <label for="inputBloodGroup" class="">Blood Group</label>
-				    <select class="form-select" id="inputBloodGroup" required>
-		                <option value="">Choose...</option>
-		                <option value="o-plus">O+</option>
-		                <option value="o-minus">O-</option>
-		                <option value="a-plus">A+</option>
-		                <option value="a-minus">A-</option>
-		                <option value="b-plus">B+</option>
-		                <option value="b-minus">B-</option>
-		                <option value="ab-plus">AB+</option>
-		                <option value="ab-minus">AB-</option>
-		            </select>
-				</div>
-				<!--
-				<div class="col-12">
-				    <label for="inputOccupation" class="">Occupation</label>
-				    <input type="text" id="inputOccupation" class="form-control" placeholder="" required>
-				</div>
-				
-				<div class="col-12">
-				    <label class="">Have you done a blood donation before?</label>
-				    <div class="form-check">
-					  <input class="form-check-input" type="radio" name="yesRadio" id="yes">
-					  <label class="form-check-label" for="yes">
-					    Yes
-					  </label>
+		<div class="col-10" style="margin: 0 auto;">
+			<form>
+				<div class="row">
+					<div class="col-12">
+					    <label for="inputName" class="">Full Name</label>
+					    <input type="text" id="inputName" class="form-control" placeholder="Full Name" required>
 					</div>
-					<div class="form-check">
-					  <input class="form-check-input" type="radio" name="yesRadio" id="no">
-					  <label class="form-check-label" for="no">
-					    No
-					  </label>
+					<div class="col-sm">
+						<label for="inputPhoneNumber" class="">Phone Number</label>
+						<input type="number" id="inputPhoneNumber" class="form-control" placeholder="Phone Number" required>
 					</div>
+					<div class="col-sm">
+						<label for="inputEmail" class="">Email Address</label>
+						<input type="email" id="inputEmail" class="form-control" placeholder="Email Address" required>
+					</div>
+					<div class="col-12">
+					    <label for="inputAddress" class="">Address</label>
+					    <textarea id="inputName" class="form-control" required></textarea>
+					</div>
+					<div class="col-12">
+					    <label for="inputAge" class="">Age</label>
+					    <input type="number" id="inputAge" class="form-control" required>
+					</div>
+					<div class="col-12">
+					    <label for="inputBloodGroup" class="">Blood Group</label>
+					    <select class="form-select" id="inputBloodGroup" required>
+			                <option value="">Choose...</option>
+			                <option value="o-plus">O+</option>
+			                <option value="o-minus">O-</option>
+			                <option value="a-plus">A+</option>
+			                <option value="a-minus">A-</option>
+			                <option value="b-plus">B+</option>
+			                <option value="b-minus">B-</option>
+			                <option value="ab-plus">AB+</option>
+			                <option value="ab-minus">AB-</option>
+			            </select>
+					</div>
+					<!--
+					<div class="col-12">
+					    <label for="inputOccupation" class="">Occupation</label>
+					    <input type="text" id="inputOccupation" class="form-control" placeholder="" required>
+					</div>
+					
+					<div class="col-12">
+					    <label class="">Have you done a blood donation before?</label>
+					    <div class="form-check">
+						  <input class="form-check-input" type="radio" name="yesRadio" id="yes">
+						  <label class="form-check-label" for="yes">
+						    Yes
+						  </label>
+						</div>
+						<div class="form-check">
+						  <input class="form-check-input" type="radio" name="yesRadio" id="no">
+						  <label class="form-check-label" for="no">
+						    No
+						  </label>
+						</div>
+					</div>
+					-->
 				</div>
-				-->
-			</div>
-		    <button class="w-100 mt-2 btn btn-primary btn-lg" type="submit">Submit</button>
-		</form>
+			    <button class="w-100 mt-2 btn btn-primary btn-lg" type="submit">Register</button>
+			</form>
+		</div>
 	</div>
 </body>
 </html>

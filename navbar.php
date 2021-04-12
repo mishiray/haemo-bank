912f77d15d4b17059fd14b31e9ef2df5f0c99a5e
Abiola Mishael <woroumishael@gmail.com>
2021-04-12 22:48:22 +0100

    arrangement

--- navbar.php
+++ navbar.php
@@ -1,11 +1,11 @@
 
-	<div class="topnav maroon" id="myTopnav">
-		<a class="navbar-brand" href="#">Haemo-Bank</a>
+	<div class="topnav purple" id="myTopnav">
+		<a class="navbar-brand active" href="#">Haemo-Bank</a>
 		<div class="float-right">
-		  <a href="index.php" class="	">Home</a>
+		  <a href="index.php" class="">Home</a>
 		  <a href="login.php" class="<?php echo (in_array($sitePage, array('login.php'))) ? 'active' : '' ?>">Login</a>
-		  <a href="main-registration.php"  class="<?php echo (in_array($sitePage, array('"main-registration.php'))) ? 'active' : '' ?> ">Register</a>
-		  <a href="#">About</a>
+		  <a href="main-registration.php"  class="<?php echo (in_array($sitePage, array('main-registration.php'))) ? 'active' : '' ?> ">Register</a>
+		  <a class="d-none" href="#">About</a>
 		  <a href="javascript:void(0);" class="icon" onclick="myFunction()">
 		    <i class="fa fa-bars"></i>
 		  </a>
